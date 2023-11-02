ad67dff1f8f1d6f63a0168860d77525a82733cfe
ivantinocoo <ivantlife432@gmail.com>
2023-11-02 12:38:04 -0700

    renamed:    create-TPCH.sql -> create-soccer_db.sql renamed:    delete-TPCH.sql -> delete-soccer_db.sql new file:   load-soccer_db.sql

---
+++ load-soccer_db.sql
@@ -0,0 +1,22 @@
+--SQLite
+DELETE FROM coaches
+DELETE FROM leagues
+DELETE FROM matches
+DELETE FROM nation
+DELETE FROM players
+DELETE FROM stats
+DELETE FROM teams
+
+--Run these commands in the .sqlite terminal
+.mode "csv"
+.seperator "|"
+--.headers off
+
+--Replace the file path with your path
+.import 
+.import
+.import
+.import 
+.import 
+.import 
+.import
