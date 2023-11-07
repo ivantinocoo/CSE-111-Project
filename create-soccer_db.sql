eb229183f9a07c4d8e93c7dc1809002ea3afa436
ivantinocoo <ivantlife432@gmail.com>
2023-11-07 11:30:21 -0800

    modified:   .DS_Store modified:   create-soccer_db.sql new file:   data/.DS_Store new file:   data/2022-2023_Football_Player_Stats.csv new file:   data/2022-2023_Football_Players.csv new file:   data/2022-2023_Football_Team_Stats.csv new file:   data/league.tbl new file:   data/nation.tbl modified:   load-soccer_db.sql modified:   soccer_db.sqlite

--- create-soccer_db.sql
+++ create-soccer_db.sql
@@ -6,29 +6,31 @@ CREATE TABLE leagues (
 
 CREATE TABLE nation (
     n_nationID              int not null,
-    n_nationName            varchar(15) not null
+    n_nationName            varchar(50) not null
 );
 
 CREATE TABLE teams (
     t_teamID                int not null,
     t_teamName              char(15) not null,
-    t_yearEstablished       varchar(152),
+    t_matchesPlayed         int not null,
     t_wins                  int not null,
-    t_losses                int not null,
     t_draws                 int not null,
-    t_coachID               int not null,
-    t_leagueID              int not null,
-    t_homeID                int not null,
-    t_awayID                int not null
+    t_losses                int not null,
+    t_goalsFor              int not null,
+    t_goalsAgainst          int not null,
+    t_goalsDifference       int not null,
+    t_Points                int not null,
+    t_topScorer             char(15) not null,
+    t_topScorerGoals        int not null
 );
 
 CREATE TABLE players (
     p_playerID              int not null,
     p_playerName            varchar(25) not null,
-    p_age                   int not null,
     p_position              varchar(4) not null,
-    p_jerseyNumber          int not null,
-    p_teamID                int not null
+    p_teamName              char(15) not null,
+    p_age                   int not null,
+    p_born                  int not null
 );
 
 CREATE TABLE matches (
@@ -45,12 +47,18 @@ CREATE TABLE coaches (
 );
 
 CREATE TABLE stats (
-    s_statsID               int not null,
     s_playerID              int not null,
-    s_matchID               int not null,
-    s_goalsScored           int not null,
-    s_assists               int not null,
+    s_matchesPlayed         int not null,
+    s_starts                int not null,
+    s_minutes               int not null,
+    s_goals                 int not null,
+    s_shots                 DECIMAL(7,2) not null,
+    s_passCompleted         DECIMAL(7,2) not null,
+    s_passAttempted         DECIMAL(7,2) not null,
+    s_assists               DECIMAL(7,2) not null,
+    s_interceptions         DECIMAL(7,2) not null,
+    s_clearences            DECIMAL(7,2) not null,
+    s_touches               DECIMAL(7,2) not null,
     s_yellowCards           int not null,
-    s_redCards              int not null,
-    s_saves                 int not null
+    s_redCards              int not null
 );
--- load-soccer_db.sql
+++ load-soccer_db.sql
@@ -1,22 +1,20 @@
 --SQLite
-DELETE FROM coaches
-DELETE FROM leagues
-DELETE FROM matches
-DELETE FROM nation
-DELETE FROM players
-DELETE FROM stats
-DELETE FROM teams
+DELETE FROM coaches;
+DELETE FROM leagues;
+DELETE FROM matches;
+DELETE FROM nation;
+DELETE FROM players;
+DELETE FROM stats;
+DELETE FROM teams;
 
 --Run these commands in the .sqlite terminal
 .mode "csv"
-.seperator "|"
+.separator ","
 --.headers off
 
 --Replace the file path with your path
-.import 
-.import
-.import
-.import 
-.import 
-.import 
-.import
+.import 'data/2022-2023_Football_Team_Stats.csv' teams
+.import 'data/nation.tbl' nation
+.import 'data/league.tbl' leagues
+.import 'data/2022-2023_Football_Players.csv' players
+.import 'data/2022-2023_Football_Player_Stats.csv' stats
