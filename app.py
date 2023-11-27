4caea60be924a4d10899295aef930e4294c23964
ivantinocoo <ivantlife432@gmail.com>
2023-11-27 15:10:34 -0800

    modified:   .DS_Store new file:   app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def openConnection(_dbFile):
+    print("++++++++++++++++++++++++++++++++++")
+    print("Open database: ", _dbFile)
+
+    conn = None
+    try:
+        conn = sqlite3.connect(_dbFile)
+        print("success")
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+    return conn
+
+def closeConnection(_conn, _dbFile):
+    print("++++++++++++++++++++++++++++++++++")
+    print("Close database: ", _dbFile)
+
+    try:
+        _conn.close()
+        print("success")
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+
+def main():
+    database = r"soccer_db.sqlite"
+
+    # create a database connection
+    conn = openConnection(database)
+    
+
+
+    closeConnection(conn, database)
+
+
+if __name__ == '__main__':
+    main()
