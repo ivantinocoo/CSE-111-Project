bbfe1c8ce8574b6cc32ea2346d3cba55e461d0a6
ivantinocoo <ivantlife432@gmail.com>
2023-11-28 14:59:25 -0800

    new file:   Coaches.html modified:   Leagues.html new file:   Matches.html modified:   Players.html modified:   Teams.html modified:   app.py deleted:    script.js modified:   style.css

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
+import os
 import sqlite3
 
 app = Flask(__name__)
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+@app.route('/Leagues.html')
+def Leagues():
+    return send_from_directory(current_directory, 'Leagues.html')
+
+@app.route('/Teams.html')
+def Teams():
+    return send_from_directory(current_directory, 'Teams.html')
+
+@app.route('/Players.html')
+def Players():
+    return send_from_directory(current_directory, 'Players.html')
+
+@app.route('/Coaches.html')
+def Coaches():
+    return send_from_directory(current_directory, 'Coaches.html')
+
+@app.route('/Matches.html')
+def Matches():
+    return send_from_directory(current_directory, 'Matches.html')
+
 DATABASE = 'soccer_db.sqlite'
 
-# Open connection function remains the same
 def open_connection():
     conn = sqlite3.connect(DATABASE)
     return conn
 
-# Close connection function remains the same
 def close_connection(conn):
     conn.close()
 
@@ -39,6 +61,24 @@ def get_players():
     players = cursor.fetchall()
     close_connection(conn)
     return jsonify({'players': players})
+
+@app.route('/api/coaches', methods=['GET'])
+def get_coaches():
+    conn = open_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM coaches")
+    coaches = cursor.fetchall()
+    close_connection(conn)
+    return jsonify({'coaches': coaches})
+
+@app.route('/api/matches', methods=['GET'])
+def get_matches():
+    conn = open_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM matches")
+    matches = cursor.fetchall()
+    close_connection(conn)
+    return jsonify({'matches': matches})
 
 if __name__ == '__main__':
     app.run(debug=True)
